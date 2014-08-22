a996a59eb32b6b350a42e659b9f3b5570d221ab1
Beluki <beluki@gmx.com>
2014-08-22 19:04:48 +0200

    Test Clang. Updated Changelog, README.

--- Source/mingw64-build.bat
+++ Source/mingw64-build.bat
@@ -2,11 +2,11 @@
 
 REM Build a 64 bit executable using MinGW64
 
-g++ 4k.cpp -O2 -s                         ^
-    -Wall -Wextra -Wpedantic -ansi        ^
-    -nostdlib -e _Z10EntryPointv          ^
-    -fno-exceptions -fomit-frame-pointer  ^
-    -mwindows                             ^
-    -lgdi32 -lkernel32 -luser32 -lwinmm   ^
+g++ 4k.cpp -O2 -s                                         ^
+    -Wall -Wextra -Wpedantic -ansi -Wmissing-declarations ^
+    -nostdlib -e _Z10EntryPointv                          ^
+    -fno-exceptions -fomit-frame-pointer                  ^
+    -mwindows                                             ^
+    -lgdi32 -lkernel32 -luser32 -lwinmm                   ^
     -o 4k
 
